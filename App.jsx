fc9f6e8fb9e88827d343244d5658dc2f2be73c00
Milorad <145986241+Mugen1988@users.noreply.github.com>
2024-03-25 19:00:34 +0100

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -7,22 +7,7 @@ export default function App() {
 		"--contrast": 1,
 		"--saturation": 1,
 	})
-	    
-/* Challenge
-
-    The range inputs don't yet do anything. Your task is to make them work as follows: 
-    
-        1. Whenever the user moves one of the range input sliders, its corresponding value in the 
-           filter state object should be updated to be the value of the input, while preserving the other two values that have not changed. 
-           
-        2. With each update of the filter state object, the --brightness, --contrast, and 
-           --saturation CSS variables that control the image's filter properties should be updated to be the values of their corresponding filter state object properties. The relevant CSS can be found on lines 1-13 of the styles.css file. 
-		                       
-        3. The initial values of the range inputs should be the inital values of their 
-		   corresponding properties in the filter state object.   
-		   
-		4. Try to make your code as DRY as possible! 
-*/
+	
 
 	function handleChange(event){
 		setFilter(prev => ({...prev, [event.target.name]: event.target.value}))
